01194476ec6288862f61ae79358607a0605ac8f0
Anuoluwa Adebayo <adebayo.anuoluwa02@gmail.com>
2024-06-11 23:48:47 +0100

    add: sharp dependency

--- next/src/components/ui/index.ts
+++ next/src/components/ui/index.ts
@@ -3,3 +3,4 @@ export { default as Nav } from "./nav";
 export { default as Section } from "./section";
 export { AboutMenu, AboutItem } from "./menu";
 export { default as Footer } from "./footer";
+export { default as ProjectCard } from "./projectcard";
