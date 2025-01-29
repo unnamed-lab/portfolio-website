5ec479a094463b3f6d22f423405f285cec229362
Adebayo Anuoluwa Success <adebayo.anuoluwa02@gmail.com>
2025-01-29 04:56:40 +0100

    feat: add Vercel Analytics for enhanced user insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import React from "react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react"
 import { Inter, Poppins } from "next/font/google";
 import { Footer, Nav } from "@/components/ui";
 import "./globals.css";
@@ -41,6 +42,7 @@ export default function RootLayout({
         <Nav />
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
